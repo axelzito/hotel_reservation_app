f19701cfb4642f689f0a820b3004991789ba1cf5
Axel Miguez <axel.d3v@gmail.com>
2023-09-06 23:11:18 +0200

    chore: add I18n

--- config/application.rb
+++ config/application.rb
@@ -30,7 +30,10 @@ module HotelReservationApp
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+    # config.time_zone = 'Central Time (US & Canada)'
+    config.i18n.default_locale = :es
+    config.i18n.load_path += Dir[Rails.root.join('config/locales/**/*.{rb,yml}').to_s]
+    config.i18n.available_locales = %i[en es pt]
     # config.eager_load_paths << Rails.root.join("extras")
 
     # Don't generate system test files.
