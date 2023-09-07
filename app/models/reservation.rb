d21c39fe2fb1053e6f61707e3b2e05b88cd5a68a
Axel Miguez <axel.d3v@gmail.com>
2023-09-07 12:37:17 +0200

    chore: add date validations

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -15,4 +15,21 @@ class Reservation
   validates :currency, :check_in, :check_out, :guest_name, presence: true
   validates :price, presence: true, format: { with: /\A\d+(?:[,.]\d{2})?\z/ }
   validates :guest_email, presence: true, format: { with: /\A[^@\s]+@[^@\s]+\z/ }
+
+  validate :check_in_not_in_past
+  validate :check_out_after_check_in
+
+  private
+
+  def check_in_not_in_past
+    return unless check_in && check_in < Time.zone.today
+
+    errors.add(:check_in, 'cannot be in the past')
+  end
+
+  def check_out_after_check_in
+    return unless check_in && check_out && check_out <= check_in
+
+    errors.add(:check_out, 'must be after the check-in date')
+  end
 end
