4feb51500ccb90456a21973185f9bd217a30373f
Axel Miguez <axel.d3v@gmail.com>
2023-09-09 18:13:58 +0200

    feat: possibility to change de language

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,9 @@ class ApplicationController < ActionController::Base
   private
 
   def set_locale
+    return I18n.locale = session[:locale] if session[:locale] && params[:locale].nil?
+
     I18n.locale = params[:locale] || I18n.default_locale
+    session[:locale] = I18n.locale
   end
 end
