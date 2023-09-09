3ea3a5946cf2f33134197915a8f20cd5b2a10c57
Axel Miguez <axel.d3v@gmail.com>
2023-09-09 23:16:50 +0200

    feat: add login view

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -31,3 +31,16 @@ $(document).ready(function() {
         $(this).val($(this).val().replace(/[^0-9.,]/g, ''));
     });
 });
+
+const passwordField = document.getElementById('password-field');
+const togglePassword = document.getElementById('toggle-password');
+
+togglePassword.addEventListener('click', function () {
+    if (passwordField.type === 'password') {
+        passwordField.type = 'text';
+        togglePassword.className = "fa fa-eye-slash";
+    } else {
+        passwordField.type = 'password';
+        togglePassword.className = "fa fa-eye";
+    }
+});
